d6523f10794e3441a3a7f387ddca9f1ab5bdaa17
John Barrett <johnjbarrett22@gmail.com>
2019-02-01 21:55:47 -0800

    Initial set up of a test file for TDD in Python

--- python+django_TDD/first_test.py
+++ python+django_TDD/first_test.py
@@ -1,2 +1,12 @@
+import unittest
+
 def sum_two(a, b):
     return a + b
+
+class MainTest(unittest.TestCase):
+
+    def test_hello(self):
+        print("Hello from the tests")
+    
+    def test_sum_two(self):
+        print("Test 2 is working")
