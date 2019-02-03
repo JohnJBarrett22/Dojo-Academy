b771ab78f5043b8b3f897525629d6cc8bffa9166
John Barrett <johnjbarrett22@gmail.com>
2019-02-02 18:34:00 -0800

    Added urls, views and fleshed out the test code to account for add, edit and delete

--- python+django_TDD/tdd_album_crud/apps/album_crud/tests.py
+++ python+django_TDD/tdd_album_crud/apps/album_crud/tests.py
@@ -1,6 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 class AlbumTest(TestCase):
 
     def test_hello(self):
         print("I am in Django!")
+
+    def test_urls(self):
+        c = Client()
+        res = c.get('/')
+        print(res)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(c.get('/add').status_code, 200)
+        self.assertEqual(c.get('/delete').status_code, 200)
+        self.assertEqual(c.get('/edit').status_code, 200)
