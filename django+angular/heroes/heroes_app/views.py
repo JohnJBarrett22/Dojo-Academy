d8b95fa605a4c70db139b675a6375bc37a98e532
John Barrett <johnjbarrett22@gmail.com>
2019-02-09 22:56:04 -0800

    Adjustments made to models.py and views.py in django+angular module

--- django+angular/heroes/heroes_app/views.py
+++ django+angular/heroes/heroes_app/views.py
@@ -1,6 +1,20 @@
 from django.http import JsonResponse
 from django.views import View
+from .models import Hero
+import json
 
-class Hero(View):
+class Heros(View):
     def get(self, request):
-        return JsonResponse({'status': 'ok'})
+        heroes = list(Hero.objects.values().all())
+        return JsonResponse({'status': 'ok', "heroes": heroes})
+
+    def post(self, request):
+        body = json.loads(request.body.decode())
+        print(body)
+        # Hero.objects.create(
+        #     alias = body['alias'],
+        #     secret = body['secret'],
+        #     description = body['description']
+        # )
+        Hero.objects.create_hero(body)
+        return JsonResponse({'status': 'ok', "message": "it was a post this time"})
